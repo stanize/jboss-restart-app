e3784422f35cf347cb19e4ebe862a1abedf6daf1
ignatius.stanislaus <stanize@gmail.com>
2025-06-18 00:03:06 +0200

    quick

--- src/main/java/com/example/jbossrestart/JBossRestartController.java
+++ src/main/java/com/example/jbossrestart/JBossRestartController.java
@@ -11,8 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class JBossRestartController {
