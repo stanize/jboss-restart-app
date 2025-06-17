a71de4ddbce392198eee8e90e1a1434caee59b7f
ignatius.stanislaus <stanize@gmail.com>
2025-06-17 21:29:23 +0200

    quick

--- src/main/java/com/example/jbossrestart/JBossRestartController.java
+++ src/main/java/com/example/jbossrestart/JBossRestartController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class JBossRestartController {
